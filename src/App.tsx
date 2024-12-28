4482e635c3ac879908216d19a1f995f81b2d66dd
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-12-28 16:21:58 -0500

    app launching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { Rect } from 'react-tela';
+import React, { useEffect, useState } from "react";
+import { Image, Text } from "react-tela";
+
+interface AppData {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  app: Switch.Application;
+}
 
 export function App() {
-	const [r, setR] = useState(0);
-	const [pos, setPos] = useState({ x: 0, y: 0 });
-	useEffect(() => {
-		function frame() {
-			setR((r) => r + 1);
-			requestAnimationFrame(frame);
-		}
-		//requestAnimationFrame(frame);
-	}, []);
-	return (
-		<Rect
-			x={pos.x}
-			y={pos.y}
-			width={400}
-			height={300}
-			fill='green'
-			rotate={r}
-			onTouchMove={(e) => {
-				const touch = e.changedTouches[0];
-				setPos({ x: touch.clientX, y: touch.clientY });
-			}}
-		></Rect>
-	);
+  const [apps, setApps] = useState<AppData[]>([]);
+
+  useEffect(() => {
+    const gap = 48;
+    const itemsPerPage = Math.floor((screen.height - gap * 2) / (64 + gap));
+    const itemsPerRow = Math.floor((screen.width - gap) / (64 + gap));
+
+    const loadApps = async () => {
+      const switchApps = Array.from(Switch.Application).filter(
+        (app) => app.icon
+      );
+      const displayedApps: AppData[] = [];
+
+      let x = gap;
+      let y = gap;
+
+      for (const app of switchApps) {
+        let img;
+        if (app.icon) {
+          img = await createImageBitmap(new Blob([app.icon]));
+        }
+        if (!img) continue;
+
+        if (x + img.width >= screen.width) {
+          x = gap;
+          y += img.height + 40 + gap;
+        }
+
+        displayedApps.push({
+          x,
+          y,
+          width: img.width,
+          height: img.height,
+          app,
+        });
+
+        x += img.width + gap;
+      }
+
+      setApps(displayedApps);
+    };
+
+    loadApps();
+  }, []);
+
+  return (
+    <>
+      {apps.map((displayedApps, index) => (
+        <React.Fragment key={index}>
+          {displayedApps.app.icon && (
+            <Image
+              src={URL.createObjectURL(new Blob([displayedApps.app.icon]))}
+              x={displayedApps.x}
+              y={displayedApps.y}
+              width={displayedApps.width}
+              height={displayedApps.height}
+              onTouchStart={() => displayedApps.app.launch()}
+            />
+          )}
+          <Text
+            x={displayedApps.x + displayedApps.width / 2}
+            y={displayedApps.y + displayedApps.height + 20}
+            fill="white"
+            fontSize={24}
+            textAlign="center"
+          >
+            {displayedApps.app.name}
+          </Text>
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
